af1ab85281cf85b925e0cd5587c975ef6bf28e57
Rohan Shah <Rohannshah2009@gmail.com>
2026-02-22 17:14:24 -0600

    Fix paapi5-nodejs-sdk bundle

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["paapi5-nodejs-sdk"],
+};
 
 export default nextConfig;
